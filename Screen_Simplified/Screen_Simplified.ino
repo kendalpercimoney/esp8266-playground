09639ef7705df36b3d6a8a82ebe202ec6314a7a3
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2023-12-07 01:12:07 +0000

    Figured out how to talk to screen

---
+++ Screen_Simplified/Screen_Simplified.ino
@@ -0,0 +1,39 @@
+//working
+
+#include "SSD1306Wire.h"
+#include "ESP8266WiFi.h"
+// Initialize the OLED display using Arduino Wire:
+SSD1306Wire display(0x3c, D6, D5); // reversed!
+
+void setup() {
+  
+  // Initialising the UI will init the display too.
+  display.init();
+
+  //display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 0, "beginning..." );
+  display.display();
+}
+
+
+void loop() {
+  // clear the display
+  display.clear();
+  int height = 10;
+  
+  display.drawString(0, 12, "Cheylin Percimoney is great.");
+  
+  //String ssid = WiFi.SSID(i);
+ 
+  //String line = ssid + ": " + rssi + " " + encryption;
+  //    display.drawString(0, y, line);
+      display.display();
+  //    delay(10);
+    
+  
+
+  // Wait a bit before scanning again
+  delay(5000);
+}
